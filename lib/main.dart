764a2337985f372209ec2058d661c6367b98aa04
Fateme Sarafraz <fatemesrfz.dev@gmail.com>
2025-11-04 11:25:48 +0330

    add provider to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/feature/product/screens/product_screen.dart';
+import 'package:flutter_state_management/providers/product_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProductScreen(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) =>ProduvtProvider(),)],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: ProductScreen(),
+      ),
     );
   }
 }
